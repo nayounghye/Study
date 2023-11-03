2a51cd30ef534cab19a5b5cc061a554b2a6ce1f4
nayounghye <yhna.dev@gmail.com>
2023-11-03 17:56:31 +0900

    수업19_실습1_sequelize 공부

--- Back-End/Node/04-Sequelize/model/User.js
+++ Back-End/Node/04-Sequelize/model/User.js
@@ -1,64 +1,44 @@
-// const mysql = require('mysql');
-// const { register } = require('../controller/Cuser');
+const mysql = require('mysql')
+const cnn = mysql.createConnection({
+  host: 'localhost',
+  user: 'user',
+  password: 'qwer1234*',
+  database: 'sesac_test',
+})
 
-// const conn = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'user',
-//   password: 'qwe123!@#',
-//   database: 'sesac_test',
-// });
-
-// exports.getUsers = (cb) => {
-//   conn.query(`SELECT * FROM user`, (err, rows) => {
-//     // err(에러) 변수가 빈 값이 아니면, 에러가 발생했다는 것으로 if문으로 에러 처리를 해줘야 한다. 아래 코드 뜻은 에러 발생시 버리겠다.
-//     if (err) {
-//       throw err;
-//     }
-//     // throw를 만나면 함수가 여기서 종료된다. 그러므로 다음 함수를 입력해줘야 한다.
-//     console.log('user', rows);
-//     cb(rows);
-//   });
-// };
-
-// exports.insertUsers = (data, cb) => {
-//   const sql = `INSERT INTO user (userid, name, pw) values ('${data.userid}','${data.name}','${data.pw}')`;
-
-//   conn.query(sql, (err, result) => {
-//     if (err) {
-//       throw err;
-//     }
-//     console.log('user insert', result);
-//     cb(result.insertId);
-//   });
-// };
-
-// ------------------ 테이블의 구조를 정의한다. ------------------
-// orm은 객체와 db와 table을 매핑하는 기술을 의미한다.
-// sequelize를 이용해서 table의 구조를 정의할 필요가 있다.
-
-function User(Sequelize, DataTypes) {
-  return Sequelize.define(
-    'user',
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      userid: {
-        allowNull: false,
-        type: DataTypes.STRING(20),
-      },
-      name: { allowNull: false, type: DataTypes.STRING(10) },
-      pw: { allowNull: false, type: DataTypes.STRING(20) },
-    },
-    {
-      tableName: 'user',
-      freezeTableName: true,
-      timestamps: false,
-    }
-  );
+exports.post_signup = (data, cb) => {
+  let sql = `INSERT INTO user(userid, name, pw) VALUES('${data.userid}','${data.name}','${data.pw}');`
+  cnn.query(sql, function (err) {
+    if (err) throw err
+    cb()
+  })
 }
 
-module.exports = User;
+exports.post_signin = (data, cb) => {
+  let sql = `SELECT * FROM user WHERE userid='${data.userid}' and pw='${data.pw}' limit 1;`
+  cnn.query(sql, function (err, rows) {
+    if (err) throw err
+    cb(rows)
+  })
+}
+exports.get_user = (id, cb) => {
+  let sql = `SELECT * FROM user WHERE id='${id}' limit 1;`
+  cnn.query(sql, function (err, rows) {
+    if (err) throw err
+    cb(rows)
+  })
+}
+
+exports.update_profile = (data, cb) => {
+  let sql = `UPDATE user SET name='${data.name}', pw='${data.pw}' WHERE id='${data.id}'`
+  cnn.query(sql, (err) => {
+    if (err) throw err
+    cb()
+  })
+}
+exports.delete_user = (id, cb) => {
+  cnn.query(`DELETE FROM user WHERE id='${id}'`, (err) => {
+    if (err) throw err
+    cb()
+  })
+}
--- Back-End/Node/04-Sequelize/routes/index.js
+++ Back-End/Node/04-Sequelize/routes/index.js
@@ -1,38 +1,17 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controller/Cuser');
+const express = require('express')
+const user = require('../controller/Cuser')
+const router = express.Router()
 
-// 나의 삽질 ----------------------------------------------------------------
-// // 로그인 페이지(index.ejs) render
-// router.get('/', controller.main);
-// // app.get('/', 위 코드로 설정함.
-// //  function (req, res) {
-// //   res.render('index');
-// // });
+router.get('/', user.index)
 
-// // 회원가입 페이지(register.ejs) render
-// router.get('/register', controller.register);
+router.get('/signup', user.signup)
+router.post('/signup', user.post_signup)
 
-// // 회원 가입 -> user table에 insert를 실행시키는 api
-// // (회원가입 버튼을 눌렀을 때 보낼 api 필요)
-// router;
+router.get('/signin', user.signin)
+router.post('/signin', user.post_signin)
 
-// // 로그인 -> user table에서 회원 존재 여부를 판단하는 api (로그인 버튼을 눌렀을 때 보낼 api 필요)
+router.post('/profile', user.profile)
+router.patch('/profile/edit/:id', user.profile_edit)
+router.delete('/profile/delete/:id', user.profile_delete)
 
-// // 회원정보 페이지 렌더
-
-// // ---------------------------------------------------------------------------------
-
-router.get('/', controller.main); // index 를 렌더
-
-router.get('/signup', controller.signup); // 회원가입 페이지를 렌더
-router.post('/signup', controller.post_signup); // 회원가입 버튼 클릭 시 post 요청을 이용해 ()안에 있는 api를 가져옴,
-
-router.get('/signin', controller.signin); // 로그인 페이지를 렌더
-router.post('/signin', controller.post_signin); // 로그인 버튼 클릭 시 post 전송
-
-router.post('/profile', controller.profile); // 프로필 페이지를 렌더 (임시로 일반 post 폼 전송을 사용, 아직 우리는 로그인을 유지시키는 기술을 안배움.)
-router.patch('/profile/edit/:id', controller.profile_edit); // 회원정보 수정 버튼 클릭 시 patch요청을 이용해 ()안에 있는 api 가져옴.
-router.delete('/profile/delete/:id', controller.profile_delete); // 회원 탈퇴 버튼 클릭 시
-
-module.exports = router;
+module.exports = router
